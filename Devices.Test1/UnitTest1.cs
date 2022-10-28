19a7fce76291c1e214bcc0a9597f49e3430c727a
David Aslan <davidaslan112671@live.se>
2022-10-28 14:27:08 +0200

    worthless test xUnitTest

---
+++ Devices.Test1/UnitTest1.cs
@@ -0,0 +1,13 @@
+namespace Devices.Test1
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+            var one = 1;
+            var two = 1;
+            Assert.Equal(one, two);
+        }
+    }
+}
