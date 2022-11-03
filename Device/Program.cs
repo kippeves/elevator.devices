cb66cde5cd8480e50ae4bb5aafd3d548fd8c9c95
Calle <calle.be@telia.com>
2022-11-03 11:50:31 +0100

    test

--- Device/Program.cs
+++ Device/Program.cs
@@ -14,8 +14,8 @@ namespace Device
         public static Task Main(string[] args)
         {
             var allElevators = new List<Elevator>();
-                allElevators.Add(new LowHeightElevator(new DeviceInfo("ebb40d70-7d01-4242-b13a-619dcc8c5bc2")));
-                allElevators.Add(new MidHeightElevator(new DeviceInfo("384cbacb-e781-4960-ac36-8dcb162a0a2b")));
+                allElevators.Add(new LowHeightElevator(new DeviceInfo("601baa30-5077-4614-a211-603e09034947")));
+                allElevators.Add(new MidHeightElevator(new DeviceInfo("08105e08-0765-4ba0-ab14-98ae77f23f3f")));
             var setup = allElevators.Select(device => device.SetupAsync()).ToArray();
             Task.WaitAll(setup);
             Console.WriteLine("Connecting elevators.");
